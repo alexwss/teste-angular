4d51623fef39d80ce69ada63e7b7023e1befb547
alexwss <alexwssilva@gmail.com>
2017-08-10 01:34:34 -0300

    adicionando a lista  array

--- js/myApp.js
+++ js/myApp.js
@@ -1,11 +1,62 @@
 var myApp = angular.module('myApp', []);
+//var teste [];
 
-myApp.controller('UserContrl', ['$scope', function ($scope) {
-                                            //Criando Namespace user details
-                                            //que nos ajudará no visual do DOM
-                                            $scope.user = {};
-                                            $scope.user.details = {"username":"Todd Motto", "id":"89110232"}        
-                                        }]);
-myApp.controller('ListaTrem',['$scope', function ($scope){
-                                            $scope.lista = [{"nome":"Alex", "idade":"25", "sexo":"Masculino"}, {"nome":"Seará?!", "idade":"17", "sexo":"Feminino"}];                                            
-                                        }]);
+myApp.controller('UserContrl', ['$scope', function($scope) {
+    //Criando Namespace user details
+    //que nos ajudará no visual do DOM
+    $scope.user = {};
+    $scope.user.details = {
+        "username": "Todd Motto",
+        "id": "89110232"
+    }
+}]);
+myApp.controller('ListaTrem', ['$scope', function($scope) {
+    
+    $scope.pessoa = {
+        nome: "",
+        idade: "",
+        sexo: ""
+    };
+    $scope.lista = [{
+        nome: "Alex",
+        idade: "25",
+        sexo: "Masculino"
+    }, {
+        nome: "Seará?!",
+        idade: "17",
+        sexo: "Feminino"
+    }];
+    console.log("teste");
+
+    $scope.vai = function() {
+        console.log("Passou aqui" + $scope.pessoa.sexo);
+        
+        
+        $scope.lista.push({
+            nome: $scope.pessoa.nome,
+            idade: $scope.pessoa.idade,
+            sexo: $scope.pessoa.sexo
+        });
+
+        console.log($scope.lista.length);
+    }
+    
+}]);
+/**
+myApp.controller('adicionaPessoa', ['$scope', function($scope) {
+    $scope.vai = function() {
+        console.log("Passou aqui" + $scope.pessoa.sexo);
+        console.log($rootScope.);
+        /*
+        $scope.lista.push({
+            nome: $scope.pessoa.nome,
+            idade: $scope.pessoa.idade,
+            sexo: $scope.pessoa.sexo
+        });
+
+        
+    }
+
+}]);
+
+**/
